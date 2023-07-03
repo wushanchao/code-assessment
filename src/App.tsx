1681572f65a3210f040ba23cd013f010d0290f89
wushanchao <wushanchao@gmail.com>
2023-07-03 22:34:17 +0800

    feat: init paganation and photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 type Photo = {
@@ -13,7 +13,7 @@ type Photo = {
   isfamily: number;
 };
 
-async function getFlickrImgs(keyword: string): Promise<Photo> {
+async function getFlickrImgs(keyword: string): Promise<any> {
   const searchApi = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=3e7cc266ae2b0e0d78e279ce8e361736&format=json&nojsoncallback=1&safe_search=1&text=${keyword}`;
   const searchRes = await fetch(searchApi);
   return searchRes.json();
@@ -21,13 +21,39 @@ async function getFlickrImgs(keyword: string): Promise<Photo> {
 
 function App() {
   const [stateSearchStr, setStateSearchStr] = useState('');
+  const [statePhotos, setStatePhotos] = useState<Photo[]>([]);
+  const [statePage, setStatePage] = useState(1);
+
+  const getImages = () => { };
 
 
   const searchImg = async (stateSearchStr: string) => {
     const data = await getFlickrImgs(stateSearchStr);
-    console.log('data', data);
+    if (!data) {
+      return;
+    }
+
+    const { stat, photos } = data;
+    if (!stat && stat !== 'ok') {
+      return;
+    }
+
+    const { page, pages, perpage, Photo, total } = photos;
+
+
   }
 
+  const onScroll = () => {
+
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    }
+  }, []);
 
 
   return (
@@ -38,7 +64,16 @@ function App() {
       <button onClick={() => {
         searchImg(stateSearchStr)
       }}>search</button>
-      <div></div>
+      <div>
+        {statePhotos.map((photo) => (
+          <img
+            key={photo.id}
+            src={`http://farm${photo.farm}.static.flickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`}
+            alt={photo.title}
+          />
+        ))}
+
+      </div>
     </div>
   );
 }
